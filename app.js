69b27c6a7ccdbf20445a96f8af2e57deea425467
Erik Akers <erikakers@gmail.com>
2015-09-09 17:42:19 -0500

    es6 updates

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
-/*-------------------------------------------------------------------------------------------------------------------*\
-|  Copyright (C) 2015 PayPal                                                                                          |
-|                                                                                                                     |
-|  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     |
-|  with the License.                                                                                                  |
-|                                                                                                                     |
-|  You may obtain a copy of the License at                                                                            |
-|                                                                                                                     |
-|       http://www.apache.org/licenses/LICENSE-2.0                                                                    |
-|                                                                                                                     |
-|  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   |
-|  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  |
-|  the specific language governing permissions and limitations under the License.                                     |
-\*-------------------------------------------------------------------------------------------------------------------*/
-
 'use strict';
 
 // make `.jsx` file requirable by node
 require('node-jsx').install();
 
-var path = require('path');
-var express = require('express');
-var renderer = require('react-engine');
+import express from 'express';
+import path from 'path';
+import ReactEngine from 'react-engine';
+import env from 'node-env-file';
 
-var app = express();
+env('./.env');
+
+const app = express();
+const http = require('http').Server(app);
+
+const port = process.env.PORT || 9000;
 
 // create the view engine with `react-engine`
-var engine = renderer.server.create({
+let engine = ReactEngine.server.create({
   routes: require(path.join(__dirname, '/public/routes.jsx')),
   routesFilePath: path.join(__dirname, '/public/routes.jsx')
 });
@@ -40,7 +31,7 @@ app.set('views', path.join(__dirname, '/public/views'));
 app.set('view engine', 'jsx');
 
 // finally, set the custom view
-app.set('view', renderer.expressView);
+app.set('view', require('react-engine/lib/expressView'));
 
 //expose public folder as static assets
 app.use(express.static(path.join(__dirname, '/public')));
@@ -60,10 +51,6 @@ app.use(function(req, res) {
   });
 });
 
-var server = app.listen(3000, function() {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+http.listen(port, function() {
+  console.log('listing on port:' + port);
 });
