c24bbf774881d064c17293b5532b9549f2bab6f9
Erik Akers <erikakers@gmail.com>
2015-09-10 18:29:46 -0500

    Adding about route and page for testing

--- public/routes.jsx
+++ public/routes.jsx
@@ -5,9 +5,11 @@ import Router from 'react-router';
 
 import App from './views/app.jsx';
 import Home from './views/home.jsx';
+import About from './views/about.jsx';
 
 var routes = module.exports = (
   <Router.Route path='/' handler={App}>
     <Router.DefaultRoute name='home' handler={Home} />
+    <Router.Route path='about' handler={About} />
   </Router.Route>
 );
---
+++ public/views/about.jsx
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+
+export default class About extends React.Component {
+
+  render() {
+    return (
+      <div id='account'>
+        <h1>{this.props.name}</h1>
+        <h6>About View</h6>
+        <a href='/some_unknown'>Click to go to an unhandled route</a>
+      </div>
+    );
+  }
+}
