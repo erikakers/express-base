b4c541e38370a03c190871d0da152ca23edbb9cc
Erik Akers <erikakers@gmail.com>
2015-09-10 18:30:09 -0500

    testing data coming out of the model passed from the controller

--- public/views/home.jsx
+++ public/views/home.jsx
@@ -8,6 +8,7 @@ export default class Home extends React.Component {
     return (
       <div id='account'>
         <h1>{this.props.name}</h1>
+        <h3>{this.props.title}</h3>
         <h6>I am a React Router rendered view</h6>
         <a href='/some_unknown'>Click to go to an unhandled route</a>
       </div>
