9ceed666fea05345ed63c73597a7ffd257258b6a
Erik Akers <erikakers@gmail.com>
2015-10-13 15:32:52 -0500

    es6 refactor

--- controllers/PagesController.js
+++ controllers/PagesController.js
@@ -1,20 +1,17 @@
-'use strict'; // eslint-disable-line
 
-class PagesController {
+export default class PagesController {
 
-  static index(req, res) {
-    res.render(req.url, {
-      title: 'React Engine Express Sample App Controller',
-      name: 'Jordan'
-    });
-  }
+    static home(req, res) {
+        res.render(req.url, {
+            title: 'React Engine Express Sample App Controller',
+            name: 'Jordan'
+        });
+    }
 
-  static about(req, res) {
-    res.render(req.url, {
-      title: 'React Engine Express Sample App form the About View.',
-      name: 'Erik'
-    });
-  }
+    static about(req, res) {
+        res.render(req.url, {
+            title: 'React Engine Express Sample App form the About View.',
+            name: 'Erik'
+        });
+    }
 }
-
-module.exports = PagesController;
