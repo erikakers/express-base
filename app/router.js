306af135cc756990dfa14dd54974416bc68969d7
Erik Akers <erikakers@gmail.com>
2015-10-13 15:32:43 -0500

    refactor to es6

--- app/router.js
+++ app/router.js
@@ -1,13 +1,13 @@
-const PagesController = require('../controllers/PagesController');
+import PagesController from '../controllers/PagesController';
 
-module.exports = function(app) {
-  app.get('/', PagesController.index);
-  app.get('/about', PagesController.about);
+module.exports = (app) => {
+    app.get('/', PagesController.home);
+    app.get('/about', PagesController.about);
 
-  app.use(function(req, res) {
-    res.render('404', {
-      title: 'React Engine Express Sample App',
-      url: req.url
+    app.use((req, res) => {
+        res.render('404', {
+            title: 'React Engine Express Sample App',
+            url: req.url
+        });
     });
-  });
 };
