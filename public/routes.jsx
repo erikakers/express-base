2ffc07f24c9f210bc720b0d3296b92d44b386a93
Erik Akers <erikakers@gmail.com>
2015-10-13 21:59:55 -0500

    clearer syntax

--- public/routes.jsx
+++ public/routes.jsx
@@ -7,9 +7,11 @@ import App from './views/app.jsx';
 import Home from './views/home.jsx';
 import About from './views/about.jsx';
 
-const routes = module.exports = ( // eslint-disable-line
+const routes = (
   <Router.Route path='/' handler={ App }>
     <Router.DefaultRoute name='home' handler={ Home } />
     <Router.Route path='about' handler={ About } />
   </Router.Route>
 );
+
+export default routes;
